ff145c60d725aa379ed67aedb956f82ad5ebc824
Sahil <sp92535@gmail.com>
2024-02-21 23:07:31 +0530

    Login SignUp Added

---
+++ Workout.md
@@ -0,0 +1,110 @@
+#Gym Workout
+    Chest=>
+        Push Ups
+        Flat Dumbell Press
+        Dumbell Fly
+        Flat Barbell Bench Press
+        Flat Machine Press
+        Incline Machine Press
+
+        Pec Dec Machine Fly                         -I
+        Cable Fly                                   -I
+        Decline Dumbell Press                       -I
+        Incline Bench Press(Barbell || Dumbell)     -I
+        Upright Front Raise(Dumbell || Cable)       -I                  
+        Chest Dips                                  -I
+
+        Chest Stretching
+
+    Shoulders=>
+        Overhead Dumbell Press
+        Dumbell Front Raise
+        Dumbell Lateral Raise
+        Overhead Machine Press
+        Shrugs
+
+        Overhead Barbell Press                      -I
+        Barbell Front Raise                         -I
+        Lateral Raise(Cable || Machine)             -I
+        Reverse Pec Dec Fly                         -I
+        Rear Delt Dumbell Fly                       -I
+        Face Pulls                                  -I
+
+        Shoulders Stretching
+    
+    Biceps=>
+        Chin Ups
+        Straight Barbell Curls
+        Dumbell Curls
+        Hammer Dumbell Curls
+        Dumbell Concentration Curls
+
+        EZ Barbell Curls                                      -I
+        Close Grip EZ Barbell Curls                           -I
+        Seated Incline Dumbell Curls                          -I
+        Cable Curls                                           -I
+        Preacher Curls(Dumbell || Barbell || Machine)         -I
+        Spider Curls(Dumbell || Barbell)                      -I
+
+        Biceps Stretching
+
+    Triceps=>
+        Push Ups
+        Skull Crushers(Neutral Grip Sleeping)
+        Skull Crushers(Neutral Grip Standing)
+        Dumbell Tricep Overhead Extension(Double Hand)
+        Dumbell Tricep Overhead Extension(Single Hand)
+        Cable Tricep Pushdown(Double Hand)
+
+        Close Grip Push Ups                             -I
+        Close Grip Flat Bench Press                     -I
+        Skull Crushers(EZ Bar Sleeping || Dumbell)      -I    
+        Cable Tricep Ovehead Extension(Single Hand)     -I               
+        Tricep Kickbacks(Dumbell || Cable)              -I
+        Cable Tricep Pushdown(Single Hand)              -I       
+        Tricep Dips                                     -I
+
+        Tricep Stretching
+
+    Back=>
+        Pull Ups
+        Chin Ups
+        Lat Pulldown
+        Close Grip Pulldown
+        Seated Lat Rowing
+
+        Deadlift                        -I                        
+        Bent Over Barbell Rows          -I          
+        Bent Over Close Grip Rows       -I                
+        Bent Over Dumbell Rows          -I            
+        Incline Chest Supported Rows    -I                    
+        Straight Arm Pulldown           -I            
+
+        Back Stretching
+    
+    Legs=>
+        Squats
+        Front Squats Machine
+        Barbell Squats
+        Leg Press
+        Leg Extension Machine
+        Hamstring Curls
+
+        Sumo Squats                 -I    
+        Lunges                      -I
+        Romanian Deadlift           -I            
+        Bulgarian Split Squat       -I                
+        Hack Squats                 -I    
+
+        Legs Stretching
+
+    Abs=>
+        Sit Ups
+        Crunches
+        Russian Twist
+        Leg Raise
+        Cable Crunch
+
+        Abs Roller          -I
+
+        Plank  
--- navbar.js
+++ navbar.js
@@ -42,24 +42,94 @@ class SpecialNav extends HTMLElement {
                         <li><a class="dropdown-item" href="Tools.html#pace">Pace Calculator</a></li>
                     </ul>
                 </li>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        More
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="Index.html#blog">Blogs</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Login</a></li>
-                        <li><a class="dropdown-item" href="#">Contact Us</a></li>
-                    </ul>
-                </li>
             </ul>
         </div>
+        
+        <div class="mx-4">
+        <button class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#LoginModal">Login</button>
+        <button class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#SignupModal">Signup</button>
+        </div>
+        
     </div>
 </nav>
+        <!-- modal -->
+
+    <!-- LoginModal -->
+    <div class="modal fade" id="LoginModal" tabindex="-1" aria-labelledby="LoginModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="LoginModalLabel">Login to SNS</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="mb-2">
+                            <label for="exampleInputEmail1" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1"
+                                aria-describedby="emailHelp">
+                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                        </div>
+
+                        <label for="inputPassword5" class="form-label">Password</label>
+                        <input type="password" id="inputPassword5" class="form-control"
+                            aria-describedby="passwordHelpBlock">
+                        <div id="passwordHelpBlock" class="form-text">
+                            Your password must be 8-20 characters long.
+                        </div>
+                        <br>
+                        <div class="mb-2">
+                            <label for="exampleInputFname" class="form-label"><a href="#">Forgot Password?</a></label>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- SignupModal -->
+    <div class="modal fade" id="SignupModal" tabindex="-1" aria-labelledby="SignupModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="SignupModalLabel">Become a member of
+                        SNS FITNESS</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="mb-2">
+                            <label for="exampleInputEmail1" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1"
+                                aria-describedby="emailHelp">
+                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                        </div>
+                        <div class="mb-2">
+                            <label for="exampleInputFname" class="form-label">Username</label>
+                            <input type="text" class="form-control" id="exampleInputUname" aria-describedby="UnameHelp">
+                            <div id="UnameHelp" class="form-text"></div>
+                        </div>
+
+                        <label for="inputPassword5" class="form-label">Password</label>
+                        <input type="password" id="inputPassword5" class="form-control"
+                            aria-describedby="passwordHelpBlock">
+                        <div id="passwordHelpBlock" class="form-text">
+                            Your password must be 8-20 characters long.
+                        </div>
+                        <br>
+                        <div class="mb-2 form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">I accept the Terms and
+                                Insurance.</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Signup</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END OF MODAL -->
         `
     }
 }
